747e05b19243251cf2817cd064a6aba7a74c307d
Yashasvi Saraswat <145708833+yashasviSaraswat@users.noreply.github.com>
2024-04-06 13:41:46 +0530

    Cycle.h

--- Cycle.h
+++ Cycle.h
@@ -8,43 +8,43 @@ namespace MenstrualCycle {
     class Cycle
     {
     protected:
-        std::string lastDate;
-        std::string currentPeriodStart;
+        string lastDate;
+        string currentPeriodStart;
         int cycleDuration;
 
     public:
         void readDates()
         {
-            std::cout << "Enter the last date of menstruation (YYYY-MM-DD): ";
-            std::cin >> lastDate;
-            std::cout << "Enter the start date of the current period (YYYY-MM-DD): ";
-            std::cin >> currentPeriodStart;
+            cout << "Enter the last date of menstruation (YYYY-MM-DD): ";
+            cin >> lastDate;
+            cout << "Enter the start date of the current period (YYYY-MM-DD): ";
+            cin >> currentPeriodStart;
         }
 
-        std::string calculateNextPeriodDate() {
+        string calculateNextPeriodDate() {
             int year, month, day;
             sscanf(currentPeriodStart.c_str(), "%d-%d-%d", &year, &month, &day);
 
-            std::tm currentPeriodTm = { 0 };
+            tm currentPeriodTm = { 0 };
             currentPeriodTm.tm_year = year - 1900;
             currentPeriodTm.tm_mon = month - 1;
             currentPeriodTm.tm_mday = day;
 
             sscanf(lastDate.c_str(), "%d-%d-%d", &year, &month, &day);
 
-            std::tm lastDateTm = { 0 };
+            tm lastDateTm = { 0 };
             lastDateTm.tm_year = year - 1900;
             lastDateTm.tm_mon = month - 1;
             lastDateTm.tm_mday = day;
 
-            std::time_t currentPeriodTime = mktime(&currentPeriodTm);
-            std::time_t lastDatePeriodTime = mktime(&lastDateTm);
+            time_t currentPeriodTime = mktime(&currentPeriodTm);
+            time_t lastDatePeriodTime = mktime(&lastDateTm);
 
             cycleDuration = (currentPeriodTime - lastDatePeriodTime) / (24 * 60 * 60);
 
             currentPeriodTime += cycleDuration * 24 * 60 * 60;
 
-            std::tm* nextPeriodTm = localtime(&currentPeriodTime);
+            tm* nextPeriodTm = localtime(&currentPeriodTime);
             char buffer[11];
             strftime(buffer, 11, "%Y-%m-%d", nextPeriodTm);
 
@@ -52,23 +52,22 @@ namespace MenstrualCycle {
         }
 
         void display() {
-            std::cout << "Last cycle: " << lastDate << std::endl;
-            std::cout << "Current cycle: " << currentPeriodStart << std::endl;
-            std::cout << "Duration: " << cycleDuration << " days" << std::endl;
+            cout << "Last cycle: " << lastDate << endl;
+            cout << "Current cycle: " << currentPeriodStart << endl;
+            cout << "Duration: " << cycleDuration << " days" << endl;
         }
 
-        void saveToFile(const std::string& filename) {
-            std::ofstream file(filename);
+        void saveToFile(const string& filename) {
+            ofstream file(filename);
             if (file.is_open()) {
-//                file << "Last cycle: " << lastDate << std::endl;
-//                file << "Current cycle: " << currentPeriodStart << std::endl;
-                file << "Expected Date: " << calculateNextPeriodDate() << std::endl;
-//                file << "Duration: " << cycleDuration << " days" << std::endl;
+                file << "Expected Date: " << calculateNextPeriodDate() << endl;
                 file.close();
+//                file << "Last cycle: " << lastDate << endl;
+//                file << "Current cycle: " << currentPeriodStart << endl;
+//                file << "Duration: " << cycleDuration << " days" << endl;
             }
-            else {
-                std::cerr << "Unable to open file for writing." << std::endl;
-            }
+            else 
+                cerr << "Unable to open file for writing." << endl;
         }
 
     };
@@ -77,9 +76,9 @@ namespace MenstrualCycle {
     {
         Cycle m;
         m.readDates();
-        std::string next = m.calculateNextPeriodDate();
+        string next = m.calculateNextPeriodDate();
         m.display();
-        std::cout << "Next expected date: " << next << std::endl;
+        cout << "Next expected date: " << next << endl;
 
         // Save data to file
         m.saveToFile("D:/cycle.txt");
